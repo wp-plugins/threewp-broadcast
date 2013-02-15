8cc6a88e9d703dcd565254734867ecc1b50afa44
edward mindreantre <edward mindreantre@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-15 04:03:46 +0000

    Broadcast v1.17
    git-svn-id: https://plugins.svn.wordpress.org/threewp-broadcast/tags/1.17@668224 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- AttachmentData.php
+++ AttachmentData.php
@@ -1,7 +1,10 @@
 <?php
 /**
- * Data of an attached file.
- 
+	@brief	Data of an attached file.
+	
+	@par	Changelog
+	
+	- @b 2013-02-14 Added extra attachment data from werk@haha.nl: attachment_title and attachment_menu_order
  */
 class AttachmentData
 {
@@ -10,7 +13,15 @@ class AttachmentData
 	private $file_metadata;			// Wordpress' metadata for the attached image.
 	private $filename_upload_dir;	// Wordpress' upload directory for this blog / file
 	private $_wp_attachment_image_alt;		// The alt value for this image, if any.
+	private $attachment_title; 		// plaatje echte naam toegevoegd
+	private $attachment_menu_order;	// plaatjes volgorde toegevoegd
 	public	$post_custom;					// Array of post meta keys and values.
+	
+	public function __construct( $options = array() )
+	{
+		foreach($options as $key => $value)
+			$this->getset($key, $value);
+	}
 	
 	public static function from_attachment_id( $attachment_id, $upload_dir )
 	{
@@ -20,16 +31,14 @@ class AttachmentData
 		$returnValue->filename_path( $upload_dir['basedir'] . '/' . $metadata['file'] );
 		$returnValue->file_metadata( $metadata );
 		
+		$attachment_data = get_post( $attachment_id );
+		$returnValue->attachment_title( $attachment_data->post_title );
+		$returnValue->attachment_menu_order( $attachment_data->menu_order );
+
 		// Copy all of the custom data for this post.		
 		$returnValue->post_custom = get_post_custom( $attachment_id );
 		
 		return $returnValue;
-	}
-	
-	public function AttachmentData($options = array())
-	{
-		foreach($options as $key => $value)
-			$this->getset($key, $value);
 	}
 	
 	private function getset( $variable, $variable_new = '' )
@@ -40,9 +49,10 @@ class AttachmentData
 			$this->$variable = $variable_new;
 	}
 	
-	public function filename_base($filename_base = '')				{	return $this->getset('filename_base', $filename_base);			}
-	public function filename_path($filename_path = '')				{	return $this->getset('filename_path', $filename_path);			}
-	public function file_metadata($file_metadata = '')				{	return $this->getset('file_metadata', $file_metadata);			}
-	public function filename_upload_dir($filename_upload_dir = '')	{	return $this->getset('file_metadata', $filename_upload_dir);	}
+	public function filename_base($filename_base = '')						{	return $this->getset('filename_base', $filename_base);					}
+	public function filename_path($filename_path = '')						{	return $this->getset('filename_path', $filename_path);					}
+	public function file_metadata($file_metadata = '')						{	return $this->getset('file_metadata', $file_metadata);					}
+	public function filename_upload_dir($filename_upload_dir = '')			{	return $this->getset('file_metadata', $filename_upload_dir);			}
+	public function attachment_menu_order($attachment_menu_order = '')		{	return $this->getset('attachment_menu_order', $attachment_menu_order);	}
+	public function attachment_title($attachment_title = '')				{	return $this->getset('attachment_title', $attachment_title);			}
 }
-?>
