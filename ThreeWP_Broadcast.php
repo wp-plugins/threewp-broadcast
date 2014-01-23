b369becc5850f19be6adecc24306e53c7b21f2c1
edward mindreantre <edward mindreantre@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-23 08:36:17 +0000

    Broadcast v2.15
    git-svn-id: https://plugins.svn.wordpress.org/threewp-broadcast/trunk@843634 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ThreeWP_Broadcast.php
+++ ThreeWP_Broadcast.php
@@ -6,7 +6,7 @@ Author URI:		http://www.plainview.se
 Description:	Broadcast / multipost a post, with attachments, custom fields, tags and other taxonomies to other blogs in the network.
 Plugin Name:	ThreeWP Broadcast
 Plugin URI:		http://plainview.se/wordpress/threewp-broadcast/
-Version:		2.14
+Version:		2.15
 */
 
 namespace threewp_broadcast;
@@ -84,7 +84,7 @@ class ThreeWP_Broadcast
 	**/
 	public $permalink_cache;
 
-	public $plugin_version = 2.14;
+	public $plugin_version = 2.15;
 
 	protected $sdk_version_required = 20130505;		// add_action / add_filter
 
@@ -2010,7 +2010,7 @@ class ThreeWP_Broadcast
 
 			// Create new post data from the original stuff.
 			$bcd->new_post = (array) $bcd->post;
-			foreach( array( 'comment_count', 'guid', 'ID', 'menu_order', 'post_parent' ) as $key )
+			foreach( [ 'comment_count', 'guid', 'ID', 'post_parent' ] as $key )
 				unset( $bcd->new_post[ $key ] );
 
 			$action = new actions\broadcasting_after_switch_to_blog;
--- readme.txt
+++ readme.txt
@@ -198,6 +198,9 @@ This plugin will soon be replaced by a WPML premium plugin that enables broadcas
 
 == Changelog ==
 
+= 2.15 20140123 =
+* Fix: Menu order is now also broadcasted.
+
 = 2.14 20140117 =
 * New: Canonical URL detects and disables the canonical link from Yoast's Wordpress SEO plugin.
 * Fix: Better compatability with User Blog Settings and Keep Child Settings plugins.
